b233acff7c135a2f2cca90ad5a3dee7794ae6cdb
ruflas <72112435+ruflas@users.noreply.github.com>
2022-09-28 16:45:26 +0200

    Update PeekingIterator.java

--- ED-PL/sesion2/src/estDatos/PeekingIterator.java
+++ ED-PL/sesion2/src/estDatos/PeekingIterator.java
@@ -2,10 +2,14 @@ package estDatos;
 
 import java.util.Iterator;
 
-public class PeekingIterator<E> implements PeekIterator<Object> {
+public class PeekingIterator<E> implements PeekIterator<Object>, Cloneable {
 	private Iterator<E> it;
 	public PeekingIterator(Iterable<E> t){
 		it = t.iterator();
+	}
+	
+	public PeekingIterator(Iterator<E> t) {
+		it = t;
 	}
 	
 	@Override
@@ -19,9 +23,17 @@ public class PeekingIterator<E> implements PeekIterator<Object> {
 	}
 
 	@Override
-	public Object peek() {
-		Iterator<E> aux = it;
-		return aux.next();
+	public Object clone(){  
+	    try{  
+	        return super.clone();  
+	    }catch(Exception e){ 
+	        return null; 
+	    }
 	}
 	
+	@Override
+	public Object peek() {
+		PeekingIterator<E> aux = new PeekingIterator<E>(it); //Es lo mismo que aux = it ???
+		return  aux.next();
+	}
 }
